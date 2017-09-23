c113cb7243f3ac48f6a2e0d45a7c2c635eb42173
Devarsh Ruparelia <devarsh.ruparelia@gmail.com>
2017-09-23 11:30:50 -0500

    added test_client

---
+++ test_client.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+import sys
+import socket
+import json
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("test-exchange-giza", 25000))
+    return s.makefile('rw', 1)
+
+def write(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+def read(exchange):
+    return json.loads(exchange.readline())
+
+
+
+
+def main():
+    exchange = connect()
+    write(exchange, {"type": "hello", "team": "TEAMNAME"})
+    hello_from_exchange = read(exchange)
+    
+    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+    
+stopBOND = false
+
+if not stopBOND:
+        bondTrader(exchange) 
+    
+
+if __name__ == "__main__":
+    main()
+
+
+

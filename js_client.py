0d6ea7c2ac9d367bc5afd13ebfd115a8c6f14e50
Devarsh Ruparelia <devarsh.ruparelia@gmail.com>
2017-09-23 12:41:44 -0500

    updated js_client with prod!

--- js_client.py
+++ js_client.py
@@ -17,12 +17,35 @@ def write(exchange, obj):
 def read(exchange):
     return json.loads(exchange.readline())
 
+def bondTrader(exchange):
+    fairPrice = 1000    #change it 1000 later
+    tradingCentral("BOND", "BUY", fairPrice -1 , 100, exchange)
+    tradingCentral("BOND", "SELL", fairPrice + 1, 100, exchange)
+
+
+def tradingCentral(symbol, direction, price, amount, exchange):
+    N = str(randint(1, 2000000000))
+    orders.append(N)
+    json_string = '{"type": "add", "order_id": '+N+',"symbol": "' + symbol +'", "dir": "' +  direction + '", "price": ' + str(price) + ', "size" : ' + str(amount) + '}'
+    print(json_string, file=sys.stderr)
+    print(json_string, file=exchange)
+
 def main():
     exchange = connect()
-    write(exchange, {"type": "hello", "team": "TEAMNAME"})
+    write(exchange, {"type": "hello", "team": "GIZA"})
     hello_from_exchange = read(exchange)
-    
-    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+    #print("The exchange replied:", hello_from_exchange, file=sys.stderr)   
+    while(1):
+        exch_book = read(exchange)
+        if ('symbol' in exch_book and exch_book['symbol'] == 'BOND'):
+            #print("Book: ", exch_book, file=sys.stderr)
+            bondTrader(exchange)
+
+
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
